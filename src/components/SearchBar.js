4893fffc9967be324e0fbef04fdb12e3779ed2de
杉山春輝 <sugiyamaharuki@sugiyamaharuakiranoMacBook-Pro.local>
2020-10-07 12:10:28 +0900

    updated label

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -17,7 +17,7 @@ const SearchBar = ({ onFormSubmit }) => {
     <div className="search-bar ui segment">
       <form onSubmit={onSubmit} className="ui form">
         <div className="field">
-          <label>Search for a Video</label>
+          <label>Search for a Video!!!</label>
           <input onChange={onInputChange} type="text" value={term} />
         </div>
       </form>
